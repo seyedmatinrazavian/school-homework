417f48d6a66edc6f68e9adf7919fdd311a2fc613
seyedmatinrazavian <matin.raa88@gmail.com>
2026-01-29 14:19:11 +0330

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,16 @@
+#now I can say how be long to be grow up
+
+
+name=input("enter your name =")
+
+
+z=18
+
+
+age=int(input("enter your age ="))
+
+
+if age <= z  :
+    print(z-age ,"years you will be grow up {} ".format(name))
+else :
+    print("you are old man {} ".format(name))
