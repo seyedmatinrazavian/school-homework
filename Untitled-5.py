50724228987020fd5c80f2171014735c60a22f9c
matin <matin.raa88@gmail.com>
2026-01-30 15:21:38 +0330

    Add files via upload
    just school homework

---
+++ Untitled-5.py
@@ -0,0 +1,50 @@
+# now enter your x and y to do some mathwork for you
+x=str(input("square=c triangle=x rectangle=z , chossse your fig="))
+m=str(input("n= area ,  v=the environment   ="))
+
+if x == "x" :
+    if m=="v" :
+     k=float(input("enter your side 1="))
+     n=float(input("enter your side 2="))
+     q=float(input("enter your side 3="))
+     print(k+n+q) 
+
+        
+if m=="n" :
+    h=float(input("enter your h ="))
+    s=float(input("enter your s ="))
+    print( "your area is" ,             h*s/2)
+   
+
+else :
+
+
+    print( "come on man i do it free for you now chosse your fig")
+
+
+if x == "c" :
+    if m=="v" :
+        o=float(input("enter your side ="))
+        print(4*o)
+
+
+        if m== "n":
+          o=float(input("enter your side ="))
+        print("your area is" ,   o*o )
+else :
+    print( "come on man i do it free for you now chosse your fig")
+
+
+if x == "z" :
+    if m=="v" :
+        p=float(input("enter your x ="))
+        g=float(input("rnter your y ="))
+        print(2*g + 2*p)
+
+
+    if m =="n" :
+     p=float(input("enter your x ="))
+     g=float(input("rnter your y ="))
+    print( "your area is" ,       p*g)
+else :
+    print( "come on man i do it free for you now chosse your fig")
